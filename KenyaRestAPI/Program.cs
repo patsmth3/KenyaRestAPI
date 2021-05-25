725de7abeac73c922a69e4583da6fa41fb96ef29
Pat Smith <patsmith@gmail.com>
2021-05-24 21:21:08 -0400

    Initial commit

---
+++ KenyaRestAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KenyaRestAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quotes.KanyeQuote();
+            
+            Quotes.RonQuote();
+        }
+    }
+}
